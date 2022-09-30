03156dc90152ad803720ba05b502a2e639c6a404
ChunWeiYeah <yher25@gmail.com>
2022-09-30 10:50:44 +0800

    測試畫面閃爍問題

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,31 @@
 import React from "react";
+import clsx from "clsx";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
-
-import Banner from "../components/Banner/Banner";
+import styles from "./index.module.css";
+// import Banner from "../components/Banner/Banner";
 import Projects from "../components/Projects/Projects";
+
+function HomepageHeader() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro"
+          >
+            Docusaurus Tutorial - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
@@ -13,7 +34,8 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-      <Banner />
+      {/* <Banner /> */}
+      <HomepageHeader />
       <main>
         <HomepageFeatures />
         <Projects />
