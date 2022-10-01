0c6654977e923b7400c4b6fad97c6a68d357b9b9
ChunWeiYeah <yher25@gmail.com>
2022-10-02 00:46:28 +0800

    檢查圖片和網址Router

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,6 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Wei Site",
+  staticDirectories: ["static"],
   tagline: "Wei Site",
   url: "https://github.com",
   baseUrl: "/Wei-Docusaurus/",
