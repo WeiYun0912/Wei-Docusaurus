f1bf64ef717fda2f670d8dd4388438c5af10469c
ChunWeiYeah <yher25@gmail.com>
2023-03-23 13:02:30 +0800

    update home page

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -9,7 +9,6 @@ import styles from "./styles.module.css";
 const ExperienceCard = ({ experience }) => {
   return (
     <VerticalTimelineElement
-      key={experience.title}
       contentStyle={{ background: "#0e0d13", color: "#fff" }}
       contentArrowStyle={{ borderRight: "7px solid #040405" }}
       date={experience.date}
@@ -49,27 +48,9 @@ const Experience = ({ Experiences }) => {
   return (
     <div className={styles.experienceContainer}>
       <VerticalTimeline>
-        <VerticalTimelineElement
-          contentStyle={{ background: "#0e0d13", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid #040405" }}
-          date={"碩士二年級"}
-        >
-          <div>
-            <h3 className={styles.experienceTitle}>碩士二年級</h3>
-            <p className={styles.experienceName} style={{ margin: 0 }}>
-              高雄科技大學 - 電子工程學系
-            </p>
-          </div>
-
-          <ul className={styles.experiencePointsContainer}>
-            <li className={styles.experiencePoint}>
-              參加 2021 第 26 屆大專校院資訊應用服務創新競賽，入圍亞太中文組。
-            </li>
-          </ul>
-        </VerticalTimelineElement>
-        {/* {Experiences.map((experience, index) => (
+        {Experiences.map((experience, index) => (
           <ExperienceCard key={index} experience={experience} />
-        ))} */}
+        ))}
       </VerticalTimeline>
     </div>
   );
